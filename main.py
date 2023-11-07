e13c6039d82528bcf37d8495fc455489e015a1a6
Andrea Valenza <andrea.valenza@prima.it>
2023-11-07 14:30:16 +0100

    Map Severities on Enums - Score as sum of severities

--- main.py
+++ main.py
@@ -8,8 +8,31 @@ import glob
 from urllib.parse import urlparse, urlencode
 import typer
 from typing_extensions import Annotated
+from enum import Enum
 
 app = typer.Typer()
+
+
+class Severity(Enum):
+
+  def __new__(cls, *args, **kwds):
+    value = len(cls.__members__)
+    obj = object.__new__(cls)
+    obj._value_ = value
+    return obj
+  def __init__(self, severity_id, score):
+    self.id = severity_id
+    self.score = score
+ 
+  EMERGENCY = 0, 0 # not used in CRS
+  ALERT     = 1, 0 # not used in CRS
+  CRITICAL  = 2, 5
+  ERROR     = 3, 4
+  WARNING   = 4, 3
+  NOTICE    = 5, 2
+  INFO      = 6, 0 # not used in CRS
+  DEBUG     = 7, 0 # not used in CRS
+
 
 @app.command()
 def version():
@@ -18,8 +41,13 @@ def version():
 
 @app.command()
 def evaluate(payload: str,
-            verbose: Annotated[bool, typer.Option(help="Print all matched rules")] = False):
+            verbose: Annotated[bool, typer.Option(help="Print matched rules with associated scores")] = False,
+            show_logs: Annotated[bool, typer.Option(help="Print libmodsecurity server logs")] = True):
     modsec = ModSecurity()
+
+    if not show_logs:
+        # disable logs
+        modsec.setServerLogCb2(lambda x, y: None, LogProperty.RuleMessageLogProperty)
 
     # FIXME temporary workaround for base URLs
     base_uri = "http://www.modsecurity.org/test"
@@ -28,6 +56,13 @@ def evaluate(payload: str,
     parsed_url = urlparse(full_url)
 
     rules = RulesSet()
+
+    # Load basic conf
+    # rules.loadFromUri("conf/modsecurity.conf")
+    rules.loadFromUri("conf/crs-setup.conf")
+    
+
+    # Load CRS
     for rule_path in glob.glob('coreruleset/rules/*.conf'):
         rules.loadFromUri(rule_path)
 
@@ -38,20 +73,20 @@ def evaluate(payload: str,
     transaction.processRequestBody()
 
     matched_rules = { m.m_ruleId:m for m in transaction.m_rulesMessages}
-    print(transaction.m_rulesMessages)
-    print([ rule.m_ruleId for rule in transaction.m_rulesMessages])
-    print([ rule.m_severity for rule in transaction.m_rulesMessages])
-    print([ rule.m_message for rule in transaction.m_rulesMessages])
+    if verbose:
+        print("# Matched rules")
+        print()
+        print("\n".join([ f" - {rule.m_ruleId}#{rule.m_ruleLine}\t[+{Severity(rule.m_severity).score}]\t- {rule.m_message}" for rule in transaction.m_rulesMessages]))
 
-    print("Total Score (from rule 949110)", matched_rules[949110].m_message.split(":")[-1].strip(" )"))
+    # print("Total Score (from rule 949110)", matched_rules[949110].m_message.split(":")[-1].strip(" )"))
 
     # print(matched_rules)
-    print("Total Score (from matched rules)", sum([ rule.m_severity for rule in matched_rules.values()]))
     if verbose:
-        print("Matched rules:", list(matched_rules.keys()))
+        print("\nTotal Score (from matched rules): ", end="")
 
-    
-
+    total_score = sum([ Severity(rule.m_severity).score for rule in transaction.m_rulesMessages])
+    print(total_score)
+    return total_score
 
     # TODO get rules scores based on paranoia level, using tags
     #  - This could be handled via modsecurity configuration
